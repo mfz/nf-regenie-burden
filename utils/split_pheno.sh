4f4e282d5f0e5cdc7e9fe219d05f1959a69b794d
mfz <zink.florian@gmail.com>
2025-09-29 12:17:09 +0000

    fix split_pheno.sh

--- utils/split_pheno.sh
+++ utils/split_pheno.sh
@@ -1,15 +1,29 @@
 #!/bin/bash
 
-input="$1"  # Replace with your actual file name
-first="$2"
-last="$3"
+# Usage: ./split_phenotype_file.sh phenotype.tsv P
 
-# Read the header and split it into an array
-IFS=$'\t' read -r -a columns < <(head -n 1 "$input")
+set -e
 
-# Loop over phenotype columns (columns 3 to end, index 2+)
-for ((i=$(($first + 1)); i<=$(($last + 1)); i++)); do
-            pheno="${columns[$i]}"
-                col_num=$((i + 1))  # 1-based indexing for cut
-                    cut -f1,2,$col_num "$input" > "${pheno}.tsv"
-            done
+INPUT_FILE="$1"
+P="$2"
+
+if [[ -z "$INPUT_FILE" || -z "$P" ]]; then
+    echo "Usage: $0 <phenotype_file.tsv> <number_of_phenotypes_per_split>"
+    exit 1
+fi
+
+# Read the header
+HEADER=$(head -n 1 "$INPUT_FILE")
+# Extract phenotype names (excluding FID and IID)
+PHENOS=($(echo "$HEADER" | cut -f3-))
+NUM_PHENOS=${#PHENOS[@]}
+
+# Compute number of chunks
+NUM_CHUNKS=$(( (NUM_PHENOS + P - 1) / P ))
+
+for ((i=0; i<NUM_CHUNKS; i++)); do
+    START=$((i * P + 3))  # +3 to skip FID and IID
+    END=$((START + P - 1))
+    OUTFILE="phenotype_split_$i.tsv"
+    cut -f1,2,"$START"-"$END" "$INPUT_FILE" > "$OUTFILE"
+done
